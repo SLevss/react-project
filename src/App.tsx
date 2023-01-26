b130a239fc3ddb1b68c8368bcafffa2967b65151
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-01-26 22:41:24 +0300

    HW #37

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import React from 'react';
-import { Timer } from './components/Timer';
+
+  return <div></div>
 
 
-function App() {
-  const flexColumn: React.CSSProperties = { display: "flex", flexDirection: "column" }
-  const flexRow: React.CSSProperties = { display: "flex", flexDirection: "row" , 
-  justifyContent: "space-around", width: "50vw", marginTop: "4vh"}
-  return <div style={flexColumn}>
-    <div style={flexRow}>
-      <Timer cityOrCountry="London"></Timer>
-      <Timer cityOrCountry="Paris"></Timer>
-    </div>
-    <div style={flexRow}>
-      <Timer cityOrCountry="India "></Timer>
-      <Timer cityOrCountry={"Cuba"}></Timer>
-    </div>
 
-  </div>
-
-}
-
-export default App;
---
+++ src/utils/random.ts
@@ -0,0 +1,4 @@
+function getRandomNumber(min: number, max: number, isMinInclusive: boolean=true, isMaxInclusive: boolean=false): number
+function getRandomMatrix(rows: number, columns: number, min: number, max: number): number[][]
+function getRandomArrayElement(array: any[]): any
+function getRandomDate(minYear: number, maxYear: number): Date
