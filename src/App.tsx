b8585b4cdea4a9f811d1432cf969401330992c28
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-01-27 21:23:17 +0300

    HW #37

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import {getRandomNumber,getRandomMatrix,getRandomArrayElement} from './utils/random';
+function App() {
+  console.log(getRandomNumber(1,50,true,false)) 
+  console.log(getRandomMatrix(50,50,1,50))
+  console.log(getRandomArrayElement([1,50,100,1000,10000]))
+  return <div >
 
-  return <div></div>
 
-
+  </div>
+  
+  }
+  export default App;
 
--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,4 +1,43 @@
-function getRandomNumber(min: number, max: number, isMinInclusive: boolean=true, isMaxInclusive: boolean=false): number
-function getRandomMatrix(rows: number, columns: number, min: number, max: number): number[][]
-function getRandomArrayElement(array: any[]): any
-function getRandomDate(minYear: number, maxYear: number): Date
+export function getRandomNumber(min: number, max: number, isMinInclusive: boolean = true, isMaxInclusive: boolean = false): number {
+  if (min === max) {
+    console.log ('min not equal to max')
+  }
+
+  if (min > max) { [min, max] = [max, min] }
+  min = isMinInclusive ? min : min + 1;
+  max = isMaxInclusive ? max : max - 1;
+  return min + Math.random() * (max - min);
+
+}
+
+export function getRandomMatrix(rows: number, columns: number, min: number, max: number): number[][] {
+  let matrix = [];
+  for (let y = 0; y < rows; y++) {
+    matrix[y] = new Array<number>(columns);
+    for (let x = 0; x < columns; x++) {
+      matrix[y][x] = getRandomNumber(min, max, true, true);
+    }
+  }
+  return matrix;
+}
+
+
+export function getRandomArrayElement(array: any[]): any {
+  return array[getRandomNumber(0, array.length, true, false)];
+}
+const nDays: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+export function getRandomDate(minYear: number, maxYear: number): Date {
+  
+  const minMonth: number=0;
+  const maxMonth: number=11;
+  const minDay: number =1;
+ 
+  const newYear: number = getRandomNumber(minYear,maxYear,true,true);
+  const newMonth: number = getRandomNumber(minMonth,maxMonth,true,true);
+  let maxDay: number = (newYear%4 === 0) ? nDays[newMonth]+1:  nDays[newMonth]; 
+  const newDay: number = getRandomNumber(minDay,maxDay,true,true);
+
+  return new Date(newYear, newMonth, newDay);
+
+}
