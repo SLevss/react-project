517b0f6b38a178b556f6d4b9b886b183a6a6c602
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-02-08 05:48:18 +0300

    HW #39

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import {getRandomNumber,getRandomMatrix,getRandomArrayElement} from './utils/random';
+
+import './App.css'
+import { CounterMultiply } from './components/CounterMultiply';
+import { CounterSquare } from './components/CounterSquare';
+import { CounterUpdater } from './components/CounterUpdater';
+import { Login } from './components/Login';
+import { Logout } from './components/Logout';
+import {useSelector} from 'react-redux';
+import { Input } from './components/Input';
 function App() {
-  console.log(getRandomNumber(1,50,true,false)) 
-  console.log(getRandomMatrix(50,50,1,50))
-  console.log(getRandomArrayElement([1,50,100,1000,10000]))
-  return <div >
-
-
+  const auth: boolean = useSelector<any, boolean>(state => state.auth.authenticated);
+  const [operand, setOperand] = React.useState(1);
+  const [factor, setFactor] = React.useState(10);
+  return <div>
+    {auth && <div>
+        <Input placeHolder={'Enter operand'} inputProcess={function (value: string):
+         string {
+        setOperand(+value);
+        return '';
+      } }></Input>
+        <Input placeHolder={'Enter factor'} inputProcess={function (value: string):
+         string {
+        setFactor(+value);
+        return '';
+      } }></Input>
+      </div>}
+    {auth && <div>
+    <CounterUpdater operand={operand}></CounterUpdater>
+    <CounterSquare></CounterSquare>
+    <CounterMultiply factor={factor}></CounterMultiply>
+  </div>}
+  {auth && <Logout></Logout>}
+  {!auth && <Login></Login>}
   </div>
-  
-  }
-  export default App;
 
+}
+export default App;
--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,43 +1,39 @@
-export function getRandomNumber(min: number, max: number, isMinInclusive: boolean = true, isMaxInclusive: boolean = false): number {
-  if (min === max) {
-    console.log ('min not equal to max')
-  }
-
-  if (min > max) { [min, max] = [max, min] }
-  min = isMinInclusive ? min : min + 1;
-  max = isMaxInclusive ? max : max - 1;
-  return min + Math.random() * (max - min);
-
+export function getRandomNumber(min: number, max: number,
+     isMinInclusive: boolean = true, isMaxInclusive: boolean = false) {
+        if (!isMinInclusive) {
+            min++;
+        }
+        if (isMaxInclusive) {
+            max++;
+        }
+        if (min == max) {
+            throw "min may not be equaled to max";
+        }
+        if (min > max) {
+            [min, max] = [max, min];
+        }
+        return min + Math.trunc(Math.random() * (max - min));
+        
 }
-
-export function getRandomMatrix(rows: number, columns: number, min: number, max: number): number[][] {
-  let matrix = [];
-  for (let y = 0; y < rows; y++) {
-    matrix[y] = new Array<number>(columns);
-    for (let x = 0; x < columns; x++) {
-      matrix[y][x] = getRandomNumber(min, max, true, true);
+export function getElement<T>(array: T[]): T {
+    return array[getRandomNumber(0, array.length)];
+    
+}
+export function getRandomMatrix(rows: number, columns: number, min: number,
+     max: number): number[][] {
+    const resMatrix: number[][] = [];
+    for (let i = 0; i < rows; i++) {
+        resMatrix[i] = [];
+        for (let j = 0; j < columns; j++) {
+            resMatrix[i][j] = getRandomNumber(min, max, true, true);
+        }
     }
-  }
-  return matrix;
+    return resMatrix;
 }
-
-
-export function getRandomArrayElement(array: any[]): any {
-  return array[getRandomNumber(0, array.length, true, false)];
-}
-const nDays: number[] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
 export function getRandomDate(minYear: number, maxYear: number): Date {
-  
-  const minMonth: number=0;
-  const maxMonth: number=11;
-  const minDay: number =1;
- 
-  const newYear: number = getRandomNumber(minYear,maxYear,true,true);
-  const newMonth: number = getRandomNumber(minMonth,maxMonth,true,true);
-  let maxDay: number = (newYear%4 === 0) ? nDays[newMonth]+1:  nDays[newMonth]; 
-  const newDay: number = getRandomNumber(minDay,maxDay,true,true);
-
-  return new Date(newYear, newMonth, newDay);
-
+    const year = getRandomNumber(minYear, maxYear);
+    const month = getRandomNumber(0, 12);
+    const day = getRandomNumber(1, 32);
+    const dateRes = new Date(year, month, day);
+    return dateRes;
 }
