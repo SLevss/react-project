75c26e2d7d93b47a34f7c340bdb460d44ba0952f
Svetlana Doskochinskaia <Light3308@gmail.com>
2023-02-10 18:48:49 +0300

    HW #40

--- src/components/navigators/Navigator.tsx
+++ src/components/navigators/Navigator.tsx
@@ -1,24 +1,23 @@
 import { NavLink, Outlet } from "react-router-dom"
 import { NavigatorProps } from './../../models/NavigatorProps'
 import './navigators.css'
-
 type NavigProps = {
     navigConfig: NavigatorProps;
 }
-
 export const Navigator: React.FC<NavigProps> = (props) => {
     function getList(): JSX.Element[] {
         return props.navigConfig.routeItems.map(item => {
             return <li className="navigator-item">
-            <NavLink style={({isActive}) => activeLink(isActive)} to={item.routPath}>{item.label}</NavLink>
-        </li>
+                <NavLink style={({ isActive }) => activingLink(isActive)} to={item.routPath}>{item.label}</NavLink>
+            </li>
         })
     }
-    function activeLink(isActive: boolean): React.CSSProperties|undefined {
+    function activingLink(isActive: boolean): React.CSSProperties | undefined {
         let result: React.CSSProperties = {};
         if (isActive) {
             result = { backgroundColor: "yellow", color: "green" }
         }
+        return result;
     }
     return <div>
         <nav>
